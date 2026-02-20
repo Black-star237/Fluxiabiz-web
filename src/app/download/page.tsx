84de259d93fca69b7aa0b60912f6fbbf0ece59dc
Black-star237 <rolandroland00035@gmail.com>
2026-02-20 17:46:20 +0100

    feat: use Finder logo for macOS downloads

--- src/app/download/page.tsx
+++ src/app/download/page.tsx
@@ -24,6 +24,16 @@ const WindowsLogo = (props: React.SVGProps<SVGSVGElement>) => (
     </svg>
 )
 
+const FinderLogo = (props: React.SVGProps<SVGSVGElement>) => (
+    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 48 48" {...props}>
+        <g fill="none" stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="4">
+            <path d="M44 38V10a2 2 0 0 0-2-2H6a2 2 0 0 0-2 2v28a2 2 0 0 0 2 2h36a2 2 0 0 0 2-2" />
+            <path d="M25 8s-5 10-4 17h6l1 15m6 0H22m8-32H18m16 8v2m-20-2v2" />
+            <path d="M13 29s4.19 3 11 3s11-3 11-3" />
+        </g>
+    </svg>
+)
+
 const platforms = [
     {
         id: "android",
@@ -55,7 +65,7 @@ const platforms = [
     {
         id: "macos",
         name: "macOS",
-        icon: AppleLogo,
+        icon: FinderLogo,
         description: "Application native pour processeurs Intel et Apple Silicon",
         available: false,
         version: "Bientôt disponible",
